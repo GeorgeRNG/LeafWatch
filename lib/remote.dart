91ab48d954ac2543a9c270027a2184b378ad2228
GeorgeRNG <grogrng@gmail.com>
2023-03-26 18:33:38 +0100

    ✨ Part of the account saving 🤭

--- lib/remote.dart
+++ lib/remote.dart
@@ -1,17 +1,28 @@
+import 'dart:convert';
+
 import 'package:dartcarwings/dartcarwings.dart';
 import 'package:leafwatch/accounts.dart';
 
 class Leaf extends Account {
   CarwingsSession session = CarwingsSession(debug: true);
 
-  Leaf._();
+  Leaf._({required super.id, required super.username, required super.password});
 
-  static Future<Leaf> login(String username, String password) async {
-    Leaf leaf = Leaf._();
+  static Future<Leaf> login(String id, String username, String password) async {
+    Leaf leaf = Leaf._(id: id, username: username, password: password);
     await leaf.session.login(username: username, password: password);
     if (leaf.session.loggedIn = true) {
       return leaf;
     }
     throw 'Login Error';
+  }
+
+  static Leaf parse(String string) {
+    return fromJson(json.decode(string));
+  }
+
+  static Leaf fromJson(dynamic data) {
+    return Leaf._(
+        id: data['id'], username: data['username'], password: data['password']);
   }
 }
