1c2b275cf93fbeccfee55621144f9e0062480647
GeorgeRNG <grogrng@gmail.com>
2023-03-26 18:32:39 +0100

    ✨ Account loading and saving

--- lib/accounts.dart
+++ lib/accounts.dart
@@ -2,49 +2,92 @@ import 'dart:convert';
 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:leafwatch/remote.dart';
+import 'package:uuid/uuid.dart';
+import 'package:uuid/uuid_util.dart';
+
+const uuid = Uuid(options: {'grng': UuidUtil.cryptoRNG});
 
 class Accounts {
   static Accounts instance = Accounts();
   FlutterSecureStorage storage = const FlutterSecureStorage();
 
-  var accounts = <Account>[];
+  bool isReady = false;
+  Map<AccountType, Map<String, Account>> accounts = {};
   Accounts() {
     storage.read(key: "leaf");
   }
 
   addAccount(AccountType type, String username, String password) async {
-    Account account;
-    if (type == AccountType.leaf) {
-      account = await Leaf.login(username, password);
+    if (isReady) {
+      if (type == AccountType.leaf) {
+        var id = uuid.v4();
+        Account account = await Leaf.login(id, username, password);
+        if (accounts[type] == null) accounts[type] = {};
+        accounts[type]?.addAll({id: account});
+        saveAccounts();
+      }
     }
-    List<String> accounts;
-    if (await storage.containsKey(key: type.name)) {
-      accounts = [account];
+  }
+
+  saveAccounts() async {
+    if (isReady) {
+      accounts.forEach((key, value) {
+        storage.write(key: key.name, value: json.encode(value));
+      });
     } else {
-      // accounts =
+      throw "Accounts aren't ready. Run [getAccounts] first.";
     }
+  }
+
+  Future<Map<AccountType, Map<String, Account>>> getAccounts() async {
+    if (!isReady) {
+      var data = await storage.readAll();
+      accounts = data.map((key, value) {
+        AccountType type =
+            AccountType.values.firstWhere((element) => element.name == key);
+
+        return MapEntry(
+            type,
+            (json.decode(value) as Map<String, dynamic>).map((id, value) =>
+                MapEntry(id, type.json(<String, String>{"id": id, ...value}))));
+      });
+      isReady = true;
+    }
+    return accounts;
   }
 }
 
 class Account {
+  String id;
+
   String username;
   String password;
 
-  Account({required this.username, required this.password});
+  Account({required this.id, required this.username, required this.password});
 
   @override
   String toString() {
-    return jsonEncode({username, password});
+    return json.encode({"id": id, "username": username, "password": password});
   }
 
-  Future<String> getData() {
-    return Future.value(toString());
+  static Account parse(String string) {
+    return fromJson(json.decode(string));
   }
+
+  static Account fromJson(dynamic data) {
+    return Account(
+        id: data['id'], username: data['username'], password: data['password']);
+  }
+
+  Map toJson() => {"id": id, "username": username, "password": password};
 }
 
 enum AccountType {
-  leaf(type: Leaf);
+  leaf(type: Leaf, parser: Leaf.parse, json: Leaf.fromJson);
 
-  const AccountType({required this.type});
+  const AccountType(
+      {required this.type, required this.parser, required this.json});
   final Type type;
+  final Account Function(String) parser;
+  final Account Function(dynamic) json;
 }
